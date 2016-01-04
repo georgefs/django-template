3cb17c779b401b5b37f957fc05888f4ab5ce5fc8
lucemia <lucemia@gmail.com>
2016-01-04 16:20:00 +0800

    switch ext from yaml to yml

--- kube/deploy-kube.sh
+++ kube/deploy-kube.sh
@@ -1,5 +1,5 @@
-kubectl create -f kube/image-pull-secret.yaml --server="119.81.189.81:8080" || true
-kubectl create -f kube/service.yaml --server="119.81.189.81:8080" || true
+kubectl create -f kube/image-pull-secret.yml --server="119.81.189.81:8080" || true
+kubectl create -f kube/service.yml --server="119.81.189.81:8080" || true
 kubectl delete rc {{ project_name }}-server-rc --server="119.81.189.81:8080" || true
-kubectl create -f kube/server-rc.yaml --server="119.81.189.81:8080"
+kubectl create -f kube/server-rc.yml --server="119.81.189.81:8080"
 kubectl get pod --server="119.81.189.81:8080"
